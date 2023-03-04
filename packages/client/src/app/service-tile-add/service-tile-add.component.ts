73cbc3678cb4da2747a394308e6e4d0e0c1b87e8
Francisco Metzger <francisco.metzger@gmail.com>
2023-03-04 01:53:16 +0100

    feat(add-service: add card to add new service

---
+++ packages/client/src/app/service-tile-add/service-tile-add.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-tile-add',
+  templateUrl: './service-tile-add.component.html',
+  styleUrls: ['./service-tile-add.component.scss'],
+})
+export class ServiceTileAddComponent {}
