7c1a14b24c4c5c9697ed9ba78ad0ad11662e1ddb
Francisco Metzger <francisco.metzger@gmail.com>
2023-03-07 23:32:53 +0100

    feat(status-check): fetch serviceInstances periodically for status updates

--- packages/client/src/app/dashboard/dashboard.component.ts
+++ packages/client/src/app/dashboard/dashboard.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ServiceInstance } from '../domain/service-instance';
 import { ServiceInstanceService } from '../domain/service-instance.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   serviceInstances?: ServiceInstance[];
+  timeInterval?: Subscription;
 
   constructor(private serviceInstanceService: ServiceInstanceService) {}
 
   ngOnInit(): void {
     this.fetchServiceInstances();
+    this.timeInterval = interval(5000).subscribe(() => this.fetchServiceInstances());
   }
 
   fetchServiceInstances() {
     this.serviceInstanceService
       .getServiceInstances()
       .subscribe(serviceInstances => (this.serviceInstances = serviceInstances));
+  }
+
+  ngOnDestroy(): void {
+    this.timeInterval?.unsubscribe();
   }
 }
