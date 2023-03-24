0b09f752fbcfb7010fd4297f6b100ef321d8dc2e
Francisco Metzger <francisco.metzger@gmail.com>
2023-03-24 21:32:53 +0100

    feat(api): add services controller

---
+++ packages/backend/Labmate.WebApi/Controllers/ServicesController.cs
@@ -0,0 +1,90 @@
+using Labmate.Persistence;
+using Labmate.Persistence.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Labmate.WebApi.Controllers;
+
+[ApiController]
+[Route("api/services")]
+public class ServicesController : ControllerBase
+{
+	private readonly LabmateContext _dbContext;
+
+	public ServicesController(LabmateContext dbContext)
+	{
+		_dbContext = dbContext;
+	}
+
+	[HttpGet]
+	[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Service>))]
+	public async Task<IActionResult> GetServices(CancellationToken cancellationToken) =>
+		Ok(await _dbContext.Services.Include(s => s.StatusCheckConfiguration).ToListAsync(cancellationToken));
+
+	[HttpGet("{id:guid}")]
+	[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Service))]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> GetService(Guid id, CancellationToken cancellationToken)
+	{
+		var service = await _dbContext.Services
+			.Include(s => s.StatusCheckConfiguration)
+			.SingleOrDefaultAsync(s => s.Id == id, cancellationToken);
+
+		return service == null
+			? NotFound()
+			: Ok(service);
+	}
+
+	[HttpPost]
+	[ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Service))]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	public async Task<IActionResult> CreateService(Service service, CancellationToken cancellationToken)
+	{
+		if (service.Id != Guid.Empty)
+		{
+			return BadRequest("Id must be empty");
+		}
+
+		_dbContext.Services.Add(service);
+		await _dbContext.SaveChangesAsync(cancellationToken);
+		return CreatedAtAction(nameof(GetService), new { id = service.Id }, service);
+	}
+
+	[HttpPut("{id:guid}")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> UpdateService(Guid id, Service service, CancellationToken cancellationToken)
+	{
+		if (id != service.Id)
+		{
+			return BadRequest("Id mismatch");
+		}
+
+		var serviceExists = await _dbContext.Services.AnyAsync(s => s.Id == id, cancellationToken);
+		if (!serviceExists)
+		{
+			return NotFound();
+		}
+
+		_dbContext.Services.Update(service);
+		await _dbContext.SaveChangesAsync(cancellationToken);
+		return NoContent();
+	}
+
+	[HttpDelete("{id:guid}")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> DeleteService(Guid id, CancellationToken cancellationToken)
+	{
+		var service = await _dbContext.Services.FindAsync(new object[] { id }, cancellationToken);
+		if (service == null)
+		{
+			return NotFound();
+		}
+
+		_dbContext.Services.Remove(service);
+		await _dbContext.SaveChangesAsync(cancellationToken);
+		return NoContent();
+	}
+}
