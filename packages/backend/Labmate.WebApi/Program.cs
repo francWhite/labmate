182f44832ae190d006261762ae7542331b104350
Francisco Metzger <francisco.metzger@gmail.com>
2023-03-31 13:03:16 +0200

    refactor(migration): move migration to dedicated method

--- packages/backend/Labmate.WebApi/Program.cs
+++ packages/backend/Labmate.WebApi/Program.cs
@@ -1,43 +1,56 @@
 using Labmate.Persistence;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Labmate.WebApi;
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
-builder.Services.AddDbContext<LabmateContext>();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public static class Program
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
-app.MapControllers();
-app.UseCors(o => o.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader());
-
-if (app.Environment.IsProduction())
-{
-	await using var scope = app.Services.CreateAsyncScope();
-	var dbContext = scope.ServiceProvider.GetRequiredService<LabmateContext>();
-	var pendingMigrations = (await dbContext.Database.GetPendingMigrationsAsync(app.Lifetime.ApplicationStopped))
-		.ToList();
-
-	if (pendingMigrations.Any())
+	public static async Task Main(string[] args)
 	{
-		app.Logger.LogInformation(
-			"Pending migrations: {PendingMigrations}",
-			pendingMigrations.Aggregate((a, b) => $"{a}, {b}"));
+		var builder = WebApplication.CreateBuilder(args);
 
-		app.Logger.LogInformation("Migrating database...");
-		await dbContext.Database.MigrateAsync(app.Lifetime.ApplicationStopped);
-		app.Logger.LogInformation("Migration complete.");
+		builder.Services.AddControllers();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+		builder.Services.AddCors();
+		builder.Services.AddDbContext<LabmateContext>();
+
+		var app = builder.Build();
+
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+
+		app.UseAuthorization();
+		app.MapControllers();
+		app.UseCors(o => o.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader());
+
+		if (app.Environment.IsProduction())
+		{
+			await MigrateDatabaseAsync(app);
+		}
+
+		await app.RunAsync();
+	}
+
+	private static async Task MigrateDatabaseAsync(WebApplication app)
+	{
+		await using var scope = app.Services.CreateAsyncScope();
+		var dbContext = scope.ServiceProvider.GetRequiredService<LabmateContext>();
+		var pendingMigrations = (await dbContext.Database.GetPendingMigrationsAsync(app.Lifetime.ApplicationStopped))
+			.ToList();
+
+		if (pendingMigrations.Any())
+		{
+			app.Logger.LogInformation(
+				"Pending migrations: {PendingMigrations}",
+				pendingMigrations.Aggregate((a, b) => $"{a}, {b}"));
+
+			app.Logger.LogInformation("Migrating database...");
+			await dbContext.Database.MigrateAsync(app.Lifetime.ApplicationStopped);
+			app.Logger.LogInformation("Migration complete.");
+		}
 	}
 }
-
-app.Run();
